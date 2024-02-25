aae5156e605ba702e1b5b184b2b5581b135fb95a
Alon-Zlicha <159768771+Alon-Zlicha@users.noreply.github.com>
2024-02-25 19:40:18 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(){
     int num1,num2;
-    printf("Please enter two integers: ");
+    printf("Please enter two integers:");
     scanf("%d %d",&num1,&num2);
     int min=num1;
     int max=num2;
@@ -11,28 +11,28 @@ int main(){
         min=num2;
         max=num1;
     }
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for(int i=min;i<=max;i++){
         if(isArmstrong(i)){
-            printf("%d ", i);
+            printf(" %d ", i);
         }
     }
-    printf("\nThe Palindromes are: ");
+    printf("\nThe Palindromes are:");
     for(int i=min;i<=max;i++){
         if(isPalindrome(i)){
-            printf("%d ", i);
+            printf(" %d ", i);
         }
     }
-    printf("\nThe prime numbers are: ");
+    printf("\nThe prime numbers are:");
     for(int i=min;i<=max;i++){
         if(isPrime(i)){
-            printf("%d ", i);
+            printf(" %d ", i);
         }
     }
-    printf("\nThe strong numbers are: ");
+    printf("\nThe strong numbers are:");
     for(int i=min;i<=max;i++){
         if(isStrong(i)){
-            printf("%d ", i);
+            printf(" %d ", i);
         }
     }
     return 0;
